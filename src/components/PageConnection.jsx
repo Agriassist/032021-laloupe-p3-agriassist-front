0a04610febb436d922adb246b425223ca44fd59b
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-06-21 10:21:04 +0200

    debut page connection

---
+++ src/components/PageConnection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PageConnection() {
+  return <div className="container__pageconnection">
+      <input type="text" placeholder="Identifiant" />
+      <input type="password" placeholder="Mot de Passe" />
+
+  </div>;
+}
