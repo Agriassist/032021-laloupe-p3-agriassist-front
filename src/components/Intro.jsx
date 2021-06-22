c057e40df22a169b69e2f911790bf28c25ed16da
axel <chapellieraxel@gmail.com>
2021-06-22 16:34:38 +0200

    fusion intro connection

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import '../Styles/intro.css'
 import logoAgri from '../logoAgri.png';
 
 function Intro() {
   const phraseIntro = 'BIENVENUE ...'.split('');
   const timer = 8
 
-  console.log(phraseIntro);
+  console.log(phraseIntro.length / timer + 1 + 's' );
 
   return (
     <div className="intro-contenair" style={{ animationDelay: phraseIntro.length / timer + 1 + 's' }}>
       <img className="intro-logo" src={logoAgri} alt="ddd" />
-      <section>
+      <section className= "intro-text">
         {phraseIntro.map((text, index) => (
           <span style={{ animationDelay: index / timer + 's' }} key={index} className="animat">{text}</span>
         ))}
