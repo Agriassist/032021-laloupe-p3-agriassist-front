fee9717a4b51af99eec63345fc6680dba9ec91f5
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-06-22 16:35:36 +0200

    test finis pour aujourd'hui

--- src/components/PageConnection.jsx
+++ src/components/PageConnection.jsx
@@ -14,7 +14,7 @@ export default function PageConnection() {
 
     const TimelineLogin = gsap.timeline();
 
-    TimelineLogin.from(refImg.current, { y: -50, duration: 0.7, delay: 0.4, opacity: 0, ease: 'power2.out' })
+    TimelineLogin.from(refImg.current, { y: -50, duration: 0.7, delay: 3.025, opacity: 0, ease: 'power2.out' })
       .from(refInputOne.current, { y: -50, duration: 0.7, opacity: 0, ease: 'power2.out' })
       .from(refInputTwo.current, { y: -50, duration: 0.7, opacity: 0, ease: 'power2.out' })
       .from(refAuthen.current, { y: -50, duration: 0.7, opacity: 0, ease: 'power2.out' });
