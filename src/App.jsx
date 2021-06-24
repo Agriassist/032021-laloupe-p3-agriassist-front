f2b0c85ef5f5c7906febb2da9a82bb77ca05b9f9
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-06-24 09:08:53 +0200

    profil

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import ParcMateriel from './components/ParcMateriel';
 import Document from './components/Document';
 import PageFin from './components/PageFin';
 import Popup from './components/Popup'
+import Profil from './components/Profil'
 
 function App() {
   return (
     <main className="container__site">
       {/* <PageConnection /> */}
-      <ParcMateriel />
+      <Profil />
     </main>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Popup.css';
 import agriculteur from '../agriculteur.png';
 
 export default function Popup() {
+  const [popup, setPopup] = useState(true);
+
+  const popupVisible = () => {
+    setPopup(!popup);
+  };
   return (
-    <div className="container">
+    <div className={popup ? 'container' : 'container__none'}>
       <div className="container__popup">
         <img src={agriculteur} alt="agriculteur" />
-        <i class="far fa-times-circle"></i>
+        <i className="far fa-times-circle" onClick={popupVisible}></i>
         <p id="popup__user">Thomas</p>
         <div className="trait__principal"></div>
         <p className="text__popup">Mon Profil</p>
@@ -18,7 +23,7 @@ export default function Popup() {
         <div className="trait__principal"></div>
         <div className="bloc__deconnection">
           <p>Deconnection</p>
-          <i class="fas fa-sign-out-alt"></i>
+          <i className="fas fa-sign-out-alt"></i>
         </div>
       </div>
     </div>
