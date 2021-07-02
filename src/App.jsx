9808e0e146d7ff7f8e57de6bcb8b9b696610868b
Benjamin-Moneta <benjamin.moneta@orange.fr>
2021-07-02 09:56:58 +0200

    Composant OneParcMateriel fini

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ import ParcMateriel from './components/ParcMateriel';
 import Document from './components/Document';
 import PageFin from './components/PageFin';
 import Popup from './components/Popup';
+import OneParcMateriel from './components/OneParcMateriel';
 
 function App() {
   return (
     <main className="container__site">
       {/* <PageConnection /> */}
       {/* <ParcMateriel /> */}
-      <MenuPrincipalAgri />
+      <OneParcMateriel />
     </main>
   );
 }
--- src/components/OneParcMateriel.jsx
+++ src/components/OneParcMateriel.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import '../Styles/OneParcMateriel.css';
+import agriculteur from '../agriculteur.png';
 
 function OneParcMateriel() {
   return (
-    <div>
-      <p>Marque: Berthoud </p>
-      <p> Modèle Raptor 4200 Ektar36</p>
-      <p>Mise en service: 02/2018</p>
-      <p> Numéro de série: XXXXX</p>
-      <p> Dernière vidange moteur: XXXh</p>
-      <p> Prochaine vidange dans XXXh</p>
-      <p> Concess prioritaire pour dépannage: Ets Cloué</p>
+    <div className="OPM_container">
+      <div className="OPM_blue_trait"></div>
+      <img className="OPM_image_profil" src={agriculteur} alt="profil_pictures"></img>
+      <p className="OPM_title">Mon Parc</p>
+      <div className="OPM_infos">
+        <p>Marque: Berthoud </p>
+        <p> Modèle Raptor 4200 Ektar36</p>
+        <p>Mise en service: 02/2018</p>
+        <p> Numéro de série: XXXXX</p>
+        <p> Dernière vidange moteur: XXXh</p>
+        <p> Prochaine vidange dans XXXh</p>
+        <p> Concess prioritaire pour dépannage: Ets Cloué</p>
+      </div>
     </div>
   );
 }
