08e730b3f0677dc2f0a702bb3ccaae9b0d1e7c3a
francigny <claire-francigny@orange.fr>
2021-07-05 12:35:13 +0200

    css allparcmateriel

--- src/components/AllParcMateriel.jsx
+++ src/components/AllParcMateriel.jsx
@@ -3,6 +3,8 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../Styles/parcMateriel.css';
+import HautDePage from './HautDePage';
 
 function AllParcMateriel(props) {
   const [infos, setInfos] = useState([]);
@@ -27,12 +29,27 @@ function AllParcMateriel(props) {
 
   return (
     <div className="container__menu">
-      {infos.map((text, index) => (
-        <span key={index}>
-          {text.serial_number}&nbsp;
-          {text.id}&nbsp;
-        </span>
-      ))}
+      <HautDePage />
+      <header className="parc-header">
+        <div className="blocMonMateriel">
+          <div className="blocMonMateriel__logo">
+            {/* <img className="imagefondparcmateriel" alt="tracesrouestracteurs" src="./src/fondparcmateriel.jpg" width="50%" /> */}
+            <i className="fas fa-tractor"></i>
+          </div>
+          <p>Mon matériel</p>
+        </div>
+      </header>
+      <div className="parc-image">
+        {infos.map((text, index) => (
+          <span className="cadre-trackteur" key={index}>
+            <img className="image-trackteur" alt={index} src="./src/images/tracteurimagemateriel.jpg" />
+            <p className="materielName">
+              {text.serial_number}&nbsp;
+              {text.id}&nbsp;
+            </p>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
