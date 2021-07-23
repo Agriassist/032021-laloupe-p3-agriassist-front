87cf62237e37316bff1b4de71de6355e7d19f5dc
axel <chapellieraxel@gmail.com>
2021-07-23 17:04:58 +0200

    update fonctionne

--- src/components/UpdateMateriel.jsx
+++ src/components/UpdateMateriel.jsx
@@ -11,7 +11,6 @@ function UpdateMateriel(props) {
   const [year, setYear] = useState('');
   const [serialNumber, setSerialNumber] = useState('');
   const [type, setType] = useState('');
-  const [typeMa, setTypeMa] = useState('');
   const [prevOil, setPrevOil] = useState('');
   const [nextOil, setNextOil] = useState('');
   const [agriculteurIdentifiant, setAgriculteurIdentifiant] = useState('');
@@ -36,12 +35,10 @@ function UpdateMateriel(props) {
         console.log(data.data);
         setInfos(data.data);
         setYear(data.data.materiel.year);
-        setSerialNumber(data.data.materiel.serialNumber);
+        setSerialNumber(data.data.materiel.serial_number);
         setType(data.data.materiel.type);
         setPrevOil(data.data.materiel.prev_oil_change);
         setNextOil(data.data.materiel.next_oil_change);
-        setAgriculteurIdentifiant('');
-        setConcessionnaireIdentifiant('');
       })
       .catch((err) => {
         alert(err);
@@ -49,40 +46,38 @@ function UpdateMateriel(props) {
   }, []);
 
   useEffect(() => {
+    let dataTableau, dataTableauPark, dataTableauModele;
     axios('http://localhost:8000/api/users')
       .then((data) => data.data)
       .then((data) => {
-        setTableau(data);
+        dataTableau = data;
+        // setTableau(data);
         console.log(data, 'donnée all users');
+        axios(`${process.env.REACT_APP_API_URL}/api/park/materiel/${props.materielId}`)
+          .then((data) => data.data)
+          .then((data) => {
+            dataTableauPark = data;
+            // setPark(data);
+            console.log(data, 'donnée users');
+            axios(`${process.env.REACT_APP_API_URL}/api/modele`)
+              .then((data) => data.data)
+              .then((data) => {
+                dataTableauModele = data;
+                // setTableauModele(data);
+                console.log(data);
+                axios(`${process.env.REACT_APP_API_URL}/api/marque`)
+                  .then((data) => data.data)
+                  .then((data) => {
+                    console.log(data);
+                    setTableauMarque(data);
+                    setTableau(dataTableau);
+                    setTableauModele(dataTableauModele);
+                    setPark(dataTableauPark);
+                  });
+              });
+          });
       });
-  }, []);
-
-  useEffect(() => {
-    axios(`${process.env.REACT_APP_API_URL}/api/park/materiel/${props.materielId}`)
-      .then((data) => data.data)
-      .then((data) => {
-        setPark(data);
-        console.log(data, 'donnée users');
-      });
-  }, []);
-
-  useEffect(() => {
-    axios(`${process.env.REACT_APP_API_URL}/api/modele`)
-      .then((data) => data.data)
-      .then((data) => {
-        setTableauModele(data);
-        console.log(data);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios(`${process.env.REACT_APP_API_URL}/api/marque`)
-      .then((data) => data.data)
-      .then((data) => {
-        setTableauMarque(data);
-        console.log(data);
-      });
-  }, []);
+  }, [infos]);
 
   function submitMateriel(e) {
     e.preventDefault();
@@ -119,46 +114,27 @@ function UpdateMateriel(props) {
       });
   }
 
-  const test = (stat) => {
-    // const result = tableau.filter((text) => {
-    //   const tri = park.filter((user) => {
-    //     if (user.users_id === text.id) {
-    //       return text.statue === stat;
-    //     }
-    //   });
-    //   return tri.length > 0 ? true : false;
-    // });
-    // // if (result[0].statue === 'agriculteur') {
-    // //   setAgriculteurId(result[0].id);
-    // // } else if (result[0].statue === 'concessionnaire') {
-    // //   setConcessionnaireId(result[0].id);
-    // // }
-    // console.log(result);
-    // return result[0].nom;
-  };
-
   useEffect(() => {
     setNextOil(parseInt(prevOil.split('h')[0], 10) + 600 + 'h');
   }, [prevOil]);
 
   useEffect(() => {
-    tableau.filter((text) => {
-      park.filter((user) => {
-        if (user.users_id === text.id) {
-          // console.log(user.users_id === text.id);
-          if (text.statue === 'agriculteur') {
-            console.log(text.nom);
-            setAgriculteurId(text.id);
-            setAgriculteurIdentifiant(text.nom);
-          } else if (text.statue === 'concessionnaire') {
-            console.log(text.nom);
-            setConcessionnaireId(text.id);
-            setConcessionnaireIdentifiant(text.nom);
+    if (tableau.length > 0 && park.length > 0) {
+      tableau.filter((text) => {
+        park.filter((user) => {
+          if (user.users_id === text.id) {
+            if (text.statue === 'agriculteur') {
+              setAgriculteurId(text.id);
+              setAgriculteurIdentifiant(text.nom);
+            } else if (text.statue === 'concessionnaire') {
+              setConcessionnaireId(text.id);
+              setConcessionnaireIdentifiant(text.nom);
+            }
           }
-        }
+        });
       });
-    });
-  }, [year]);
+    }
+  }, [park, tableau]);
 
   return (
     <div className="container_materiel_creation">
@@ -174,7 +150,6 @@ function UpdateMateriel(props) {
                   className="select__marque"
                   defaultValue="..."
                   onChange={(e) => {
-                    setTypeMa(e.target.value);
                     setMarqueId(e.target.selectedOptions[0].id);
                   }}>
                   <option key="0" id="0">
@@ -191,7 +166,6 @@ function UpdateMateriel(props) {
                   className="select__modele"
                   defaultValue="..."
                   onChange={(e) => {
-                    setType(e.target.value);
                     setModeleId(e.target.selectedOptions[0].id);
                   }}>
                   <option key="0" id="0">
